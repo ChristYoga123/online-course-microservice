ebbdb0f2b8f1f7a2ada5649141459d156656ac92
ChristYoga123 <christianuswibisono@gmail.com>
2024-08-18 07:30:00 +0700

    feat: setup api gateway

--- be/api-gateway/routes/media.js
+++ be/api-gateway/routes/media.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('user route');
+    res.send('media route');
 });
 
 module.exports = router;
